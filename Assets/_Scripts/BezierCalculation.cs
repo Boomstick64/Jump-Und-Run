3dce74d02e63b02fd6b73be4c4c4d2a89cfa861c
Boomstick64 <71123964+Boomstick64@users.noreply.github.com>
2021-05-23 11:39:00 +0930

    Bezier calculation stuff

--- Assets/_Scripts/BezierCalculation.cs
+++ Assets/_Scripts/BezierCalculation.cs
@@ -21,30 +21,31 @@ public class BezierCalculation : MonoBehaviour
     {
         // DrawQuadraticCurve();
 
-        Debug.Log(CalculateQuadraticBezierPoint(1, point0.position, point1.position, point2.position));
+        // Debug.Log(CalculateQuadraticBezierPoint(1, point0.position, point1.position, point2.position));
     }
 
-    private void DrawQuadraticCurve()
-    {
-        for (int i = 1; i < numPoints + 1; i++)
-        {
-            float t = i / (float)numPoints;
-            positions[i - 1] = CalculateQuadraticBezierPoint(t, point0.position, point1.position, point2.position);
-        }
-        lineRenderer.SetPositions(positions);
-    }
+    //private void DrawQuadraticCurve()
+    //{
+    //    for (int i = 1; i < numPoints + 1; i++)
+    //    {
+    //        float t = i / (float)numPoints;
+    //        positions[i - 1] = CalculateQuadraticBezierPoint(t, point0.position, point1.position, point2.position);
+    //    }
+    //    lineRenderer.SetPositions(positions);
+    //}
 
-    private Vector3 CalculateQuadraticBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
-    {
-        // return = (1-t)* itself * P0 + 2*(1-t)*t*P1 + t * itself * P2
-        //            u                      u            tt
-        //           uu * P0 + 2 * u * t * P1 + tt * P2
-        float u = 1 - t;
-        float tt = t * t;
-        float uu = u * u;
-        Vector3 p = uu * p0;
-        p += 2 * u * t * p1;
-        p += tt * p2;
-        return p;
-    }
+    //private float CalculateQuadraticBezierPoint(Vector3 p, Vector3 p0, Vector3 p1, Vector3 p2)
+    //{
+    //    // return = (1-t)* itself * P0 + 2*(1-t)*t*P1 + t * itself * P2
+    //    //            u                      u            tt
+    //    //           uu * P0 + 2 * u * t * P1 + tt * P2
+    //    float t;
+    //    float u = 1 - t;
+    //    float tt = t * t;
+    //    float uu = u * u;
+    //    p = uu * p0;
+    //    p += 2 * u * t * p1;
+    //    p += tt * p2;
+    //    return t;
+    //}
 }
